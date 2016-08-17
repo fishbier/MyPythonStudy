c3c80023cd531b1e19f2728a0747bf24dec7790b
Biyu Hua <fishbier@gmail.com>
2016-08-16 23:23:36 -0700

    add ex4.py and edit ex3.py

--- ex3.py
+++ ex3.py
@@ -5,8 +5,8 @@ print "Roosters 100 - 25 * 3 % 4", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6 # 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
-
+print "3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6=",3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+print "1/4 =",1 / 4
 print "Is it true that 3 + 2 < 5 - 7?"
 
 print 3 + 2 < 5 - 7 # 3 + 2 < 5 - 7
