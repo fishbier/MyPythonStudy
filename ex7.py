14f325f4f4ddcf495ca1fbc41fec774f4c3f4eb2
Biyu Hua <fishbier@gmail.com>
2016-08-19 23:23:23 -0700

    add ex6 and ex7

---
+++ ex7.py
@@ -0,0 +1,37 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+a1 = "N"
+a2 = "u"
+a3 = "L"
+a4 = "o"
+a5 = "v"
+a6 = "e"
+a7 = "s"
+a8 = "B"
+a9 = "i"
+
+print a1 + a2,
+print a3 + a4 + a5 + a6 + a7,
+print a8 + a9
+
+print ":)" * 8
