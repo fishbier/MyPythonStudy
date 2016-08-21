6b1bc21871aec90a1b7c13b959e8e2887009fd4c
Biyu Hua <fishbier@gmail.com>
2016-08-21 11:33:39 -0700

    add ex9 and ex10

---
+++ ex10.py
@@ -0,0 +1,21 @@
+# \ escape sequence is to escape a single-quote or douuble-quote
+# I am 6'2\" tall." # escape double-quote inside string
+# The second way is by using tripe-quotes, which is just""" and works like a string,
+# but you also can put as many lines of text as you want until you type """
+# again. We'll also play with these
+
+tabby_cat = "\tI'm tabbled in." # \t means horizontale Tab (TAB)
+persian_cat = "I'm split\non a line." # \n means ASCII linefeed (LF)
+backslash_cat = "I'm \\ a \\ cat." # \\ means \
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
