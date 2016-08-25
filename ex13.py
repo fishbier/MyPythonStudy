1424a8c82fe822329f5734c27e64bcf5f0246521
Biyu Hua <fishbier@gmail.com>
2016-08-24 23:18:48 -0700

    add ex13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv # argv is argument variable. sys module
+
+script, first, second, third = argv # "unpacks" argv, take whatever is in argv, unpack it, and assign it to all of these variable on the left in order
+
+print "The script is called:", script 
+print "Your first variable is:", first 
+print "Your second variable is:", second 
+print "Your third variable is:", third 
